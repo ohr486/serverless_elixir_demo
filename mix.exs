822a1f05a266fee420445c0514735ef61985785f
ohr486 <ohr486@gmail.com>
2019-06-02 13:50:26 +0900

    update ex ver

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ServerlessElixirDemo.MixProject do
     [
       app: :serverless_elixir_demo,
       version: "0.0.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
